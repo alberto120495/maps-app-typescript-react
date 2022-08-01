c14d8a4d59e671400d2b138968f03b25f3db9107
AlbertoPimentel <albertopimentel@gmail.com>
2022-08-01 17:39:48 -0500

    places reducer

--- src/contexts/places/PlacesProvider.tsx
+++ src/contexts/places/PlacesProvider.tsx
@@ -1,5 +1,6 @@
-import { useContext } from "react";
+import { useReducer } from "react";
 import { PlacesContext } from "./PlacesContext";
+import placesReducer from "./placesReducer";
 
 export interface PlacesState {
   isLoading: boolean;
@@ -16,11 +17,13 @@ interface Props {
 }
 
 export const PlacesProvider = ({ children }: Props) => {
+    
+  const [state, dispatch] = useReducer(placesReducer, INITIAL_STATE);
+
   return (
     <PlacesContext.Provider
       value={{
-        isLoading: true,
-        userLocation: undefined,
+        ...state,
       }}
     >
       {children}
--- src/contexts/places/placesReducer.ts
+++ src/contexts/places/placesReducer.ts
@@ -1,6 +1,18 @@
+import { PlacesState } from "./PlacesProvider"
 
-function placesReducer() {
-  
+type placesAction = {
+    type:"setUserLocation", payload: [number, number]
+}
+
+function placesReducer(state:PlacesState, action:placesAction):PlacesState {
+
+  switch (action.type) {
+    case "setUserLocation":
+        return {...state, isLoading:false, userLocation:action.payload}
+     
+    default:
+        return state;
+  }
 }
 
 export default placesReducer
